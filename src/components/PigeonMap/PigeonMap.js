a45af53f579071d79476a104b982f9219d94ce7f
Casey Jordan <Jordan_Casey.bah.com>
2017-12-18 14:07:44 -0500

    added marker functionality

--- src/components/PigeonMap/PigeonMap.js
+++ src/components/PigeonMap/PigeonMap.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import Map from 'pigeon-maps'
+import Marker from 'pigeon-marker'
 
+const markers = [
+  {
+    name: 'Kottbusser Tor',
+    latlng: [38.9072, -77.0369]
+  }
+]
 
 
 class PigeonMap extends Component {
 
+  onMarkerClick(e) {
+    console.log(e.payload)
+  }
+
   render(){
+
+    const PigeonMarkers = markers.map(marker => (
+      <Marker key={`marker_${marker.name}`} anchor={marker.latlng} payload={marker.name} onClick={this.onMarkerClick} />
+    ));
+
     return(
       <div className="map">
        <Map
@@ -14,6 +30,7 @@ class PigeonMap extends Component {
          defaultCenter={[38.9072, -77.0369]}
          defaultZoom={13}
        >
+       {PigeonMarkers}
        </Map>
      </div>
     )
